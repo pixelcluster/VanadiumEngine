cd3a3bb30218721eb5cfcbde249ac29d434aca4f
pixelcluster <83122513+pixelcluster@users.noreply.github.com>
2022-03-15 14:49:05 +0100

    Asset library structs

--- include/graphics/assets/AssetLibrary.hpp
+++ include/graphics/assets/AssetLibrary.hpp
@@ -1,11 +1,45 @@
+#include <filesystem>
+#include <vector>
 
+#define VK_NO_PROTOTYPES
+#include <vulkan/vulkan.h>
 
 namespace vanadium::graphics {
+    struct LibraryBuffer
+    {
+        uint64_t id;
+        void* data;
+        size_t dataSize;
+    };
+
+    struct LibraryImageLOD
+    {
+        uint64_t id;
+        uint32_t width;
+        uint32_t height;
+        VkFormat format;
+    };
+    
+    struct LibraryImage
+    {
+        uint64_t id;
+        std::vector<LibraryImageLOD> lods;
+    };
+
+    struct LibraryMesh
+    {
+        uint64_t id;
+        std::vector<LibraryBuffer> lods;
+    };
+
     class AssetLibrary
     {
     public:
-        AssetLibrary(/* args */);
+        AssetLibrary(const std::string& libraryFile);
     private:
+        std::string m_libraryFileName;
+        void* libraryData;
+
     };
     
 }
